9d533971fefd70d3baf4b5e773f115f30a545486
Shashwat pandey <shashwat.pandey2021@vitbhopal.ac.in>
2024-06-05 11:57:09 +0530

    First commit

---
+++ mergeTwoLists.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int val){
+        this.val = val;
+    }
+
+}
+public class mergeTwoLists{
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while(l1!=null && l2!= null){
+            if(l1.val<=l2.val){
+                curr.next = l1;
+                l1 = l1.next;
+            }else{
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+
+        }
+        curr.next = l1!=null ? l1:l2;
+        return dummy.next;
+    }
+
+    public static void main(String[] args){
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(3);
+        l1.next.next = new ListNode(5);
+
+        ListNode l2 = new ListNode(2);
+        l2.next = new ListNode(4);
+        l2.next.next = new ListNode(7);
+
+        Solution sol = new Solution();
+        ListNode mergedLists = sol.mergeTwoLists(l1, l2);
+
+        while(mergedLists!=null){
+            System.out.print(mergedLists.val + " ");
+            mergedLists = mergedLists.next;
+        }
+    }
+}
